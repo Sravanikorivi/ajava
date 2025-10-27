4f341d119b34fb3241c29696218d03eac84b2dbe
Sravanikorivi <korivisravani11@gmail.com>
2025-10-27 15:41:06 +0530

    Add files via upload

---
+++ exp-4 operations.java
@@ -0,0 +1,23 @@
+package operations;
+import java.sql.*;
+public class Sravani {
+
+	public static void main(String[] args)throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:XE","system","tec");
+		Statement stmt=con.createStatement(
+				ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		ResultSet rs=stmt.executeQuery("select age from student1258");
+		rs.absolute(1);
+		int newage = rs.getInt("age")+1;
+		rs.updateInt("age", newage);
+		rs.updateRow();
+		System.out.println(newage);
+		rs.close();
+		stmt.close();
+		con.close();
+				
+	} 
+
+}
